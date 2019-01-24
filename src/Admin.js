144b5725c902ebd268b95a5f8681963a48f21a77
company-imac <hefkang@gmail.com>
2019-01-24 17:01:59 +0800

    nav-left initially completed

--- src/Admin.js
+++ src/Admin.js
@@ -3,19 +3,20 @@ import {Row,Col} from 'antd'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Navleft from './components/NavLeft';
+import './style/common.less'
 
 
 export default class Admin extends Component{
     render(){
         return (
             <div>
-                <Row>
-                    <Col span={3}>
+                <Row className="container">
+                    <Col span={3} className="nav-left">
                         <Navleft/>
                     </Col>
-                    <Col span={21}>
+                    <Col span={21} className="main">
                         <Header/>
-                        <Row>
+                        <Row className="content">
                             <Col span={24}>
                             content
                             </Col>
